ccf2e693fa998d186febb176502a52a7ac2fe9ad
hyejin <obndo8539@naver.com>
2022-12-20 21:05:38 +0900

    12.19수업-postman 사용, Controller 수정

---
+++ src/main/java/com/study/springboothyejin/web/controller/DBTestController.java
@@ -0,0 +1,29 @@
+package com.study.springboothyejin.web.controller;
+
+import com.study.springboothyejin.service.UserService;
+import com.study.springboothyejin.web.dto.UserDto;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/db/test")
+public class DBTestController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity<?> createUser(@RequestBody UserDto userDto){
+        userService.addUser(userDto);
+        return ResponseEntity.created(null).body(true);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable int userId){
+        UserDto userDto = userService.getUser(userId);
+        return ResponseEntity.ok().body(userDto);
+    }
+
+}
